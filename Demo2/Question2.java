b116db77a47f6c2f0bfda3cf530652965edee050
Jermyn <rhemus_lupin@hotmail.com>
2017-08-28 00:50:42 +0800

    Shown do-while loop

--- Demo2/Question2.java
+++ Demo2/Question2.java
@@ -10,15 +10,19 @@ public class Question2
         Scanner scanString;
         scanString = new Scanner(s);
 
-        int index = (int) (Math.random()*4) + 1; //cast this to get the 4
+        int index = (int) (Math.random()*4); //cast this to get the 4
         System.out.println(index);
         String word = "";
+        int length;
+        int guess = 0;
+        int again = 0;
         
-        for(int n=1; n<=index; n++)
+        for(int n=0; n<index; n++)
         {
             word = scanString.next();
         }
         System.out.println(word);
+        length = word.length();
 
         StringBuffer answer = new StringBuffer(word); //want to use so that user can guess when i put as ---
         //        System.out.println(reader.next());
@@ -29,16 +33,22 @@ public class Question2
         }
         System.out.println(answer);
 
-        System.out.print("Enter a letter: ");
-        char c = console.nextLine().charAt(0);
-        for(int n=0; n<answer.length(); n++)
-        {
-            if (word.charAt(n) == c)
+        do {
+            System.out.print("Enter a letter: ");
+            char c = console.nextLine().charAt(0);
+            for(int n=0; n<answer.length(); n++)
             {
-                answer.setCharAt(n,c);
+                if (word.charAt(n) == c)
+                {
+                    answer.setCharAt(n,c);
+                    guess = 1;
+                }
             }
-        }
-        System.out.println(answer);
-        
+            System.out.println(answer);
+        } while(guess==0); //why do a do-while loop?
+
+        /**Need to do a do-while loop to ask user if he wants to spell the word out.
+        If he wants, get him to spell the complete word. Feedback to him if its correct.
+        If he doesnt want, get him to enter another letter.
     }
 }
